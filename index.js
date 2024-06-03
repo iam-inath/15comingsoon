30bdb6338e6bc0ed362fe18cfaeda5a71190c7f3
Nathan Katshi <nathan@katshi.dev>
2024-06-03 12:03:59 +0200

    features update
    - disabled form

---
+++ index.js
@@ -0,0 +1,8 @@
+const email = document.querySelector('.email').value
+const subscribeBtn = document.querySelector('.subscribe')
+
+subscribeBtn.addEventListener('click', () => {
+   getNotified()
+})
+
+function getNotified() {}
